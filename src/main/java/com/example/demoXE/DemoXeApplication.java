b6dfca99642f6c5ba47d6a82817a20542a215160
Paphangkorn <chlaemnak@gmail.com>
2019-05-31 23:25:08 +0700

    first commit

---
+++ src/main/java/com/example/demoXE/DemoXeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoXE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoXeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoXeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoXE/DemoXeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoXE;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoXeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
